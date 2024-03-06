5bae2acbfbe1d6e3595069e66cb8df8d2ff92ea5
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2024-03-05 23:16:36 -0300

    Número de telefone removido na request de sign-up e da tabela Account

--- Core/Services/Auth/AuthService.cs
+++ Core/Services/Auth/AuthService.cs
@@ -36,12 +36,7 @@ namespace Api.Services.Auth
 
         public async Task<Infrastructure.Models.Account> SignUp(SignUpRequest request)
         {
-            Infrastructure.Models.Account account = new(
-                request.Auth0Id,
-                request.CPF,
-                request.BirthDate,
-                request.PhoneNumber
-            );
+            Infrastructure.Models.Account account = new(request.Auth0Id, request.CPF, request.BirthDate);
 
             await ThrowExceptionIfSignUpIsInvalid(account, request.IsTOSAccepted);
 
@@ -51,13 +46,9 @@ namespace Api.Services.Auth
                 return account;
             }
 
-            Customer? stripeAccount = await stripeCustomerService.CreateAsync(new CustomerCreateOptions
-            {
-                Phone = account.PhoneNumber
-            });
+            Customer? stripeAccount = await stripeCustomerService.CreateAsync(new CustomerCreateOptions());
 
             account.StripeCustomerId = stripeAccount.Id;
-
             genericRepository.Add(account);
 
             return account;
@@ -69,13 +60,12 @@ namespace Api.Services.Auth
             {                
                 if (!isTOSAccepted)
                     throw new BadRequestException("Os termos de uso precisam ser aceitos.");
-
                 if (await accountRepository.CPFExists(account.CPF))
                     throw new BadRequestException("Um usuário com esse CPF já está cadastrado na plataforma.");
             }
             catch (Exception e)
             {
-                logger.LogError($"Ocorreu um erro tentar validar se o usuário {account.Id} já está cadastrado" +
+                logger.LogError($"Ocorreu um erro tentar validar se o usuário {account.Id} já está cadastrado " +
                     $"na plataforma. {e.Message}");
                 throw;
             }
