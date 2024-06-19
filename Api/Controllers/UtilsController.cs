cbcb3d6fc48317b57f759296dd34229233c75c44
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2024-06-19 17:54:30 -0300

    API de health check da B3.

--- Api/Controllers/UtilsController.cs
+++ Api/Controllers/UtilsController.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Api.Clients.B3;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
 
 namespace Api.Controllers
 {
     [AllowAnonymous]
     public class UtilsController : BaseController
     {
+        private readonly IB3Client b3Client;
+
+        public UtilsController(IB3Client b3Client)
+        {
+            this.b3Client = b3Client;
+        }
+
         [HttpGet("ping")]
         public IActionResult PingPong()
         {
             return Ok("pong!");
         }
+
+        [HttpGet("ping/b3")]
+        public async Task <IActionResult> B3PingPong()
+        {
+            var statusCode = await b3Client.B3HealthCheck();
+            return Ok(new { statusCode = statusCode });
+        }
+
     }
 }
